9aa9eeedd41e121e464f1e96b346349befd3ec96
yakushiji <yaoshisizhenghe@gmail.com>
2013-08-25 15:11:58 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CapybaraTest::Application.config.secret_token = 'c87ee2010aabd5bd71c4f409632cfb91c52fd96c281c1b9cf2e159686e7f8eaa0b65f807c8e8de81659e46e2bbb1d100fe65a76a3b7a906e4c2ee82ba1214561'
